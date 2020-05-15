f2d7d3248f834e56a0ff8f6a87c9c9747049c745
cristiann.grajales <cristiann.grajales@gmail.com>
2020-05-14 23:49:15 -0500

    Correción de los archivos necesarios para el uso de boostrap

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,12 +2,13 @@ const mix = require('laravel-mix');
 
 mix.scripts([
     'resources/js/assets/jquery.js',
+    'resources/js/assets/bootstrap.js',
     'resources/js/assets/toastr.js',
     'resources/js/assets/axios.js',
     'resources/js/assets/fontawesome.js',
     'resources/js/assets/app.js',
 ], 'public/js/app.js')
    .styles([
-       'resources/sass/app.scss',
+       'resources/css/bootstrap.css',
        'resources/css/toastr.css',
 ], 'public/css/app.css');
